85d52429886e2d808b73675e0b538b61849ddf96
DotShark <arnaud.petit4@gmail.com>
2024-02-14 01:53:22 +0100

    Finished project page

--- app/lib/projects.ts
+++ app/lib/projects.ts
@@ -7,7 +7,13 @@ export type Project = {
   imagePath: string,
   shortDescription?: string,
   longDescription?: string,
-  usedSkills: Skill[]
+  usedSkills: Skill[],
+  links?: ProjectLink[]
+};
+
+export type ProjectLink = {
+  text: string,
+  url: string
 };
 
 export const projects: Project[] = [
@@ -17,6 +23,23 @@ export const projects: Project[] = [
     period: ["Novembre 2023", "Janvier 2024"],
     imagePath: "/projects/techtutor.jpg",
     shortDescription: "TechTutor est une application qui a pour objectif de combattre la fracture numérique en proposant des cours d’informatiques accessibles et ludiques à toutes et à tous.",
+    longDescription:
+      "## TechTutor, qu'est-ce que c'est ?\n" +
+      "Tech Tutor est une application conçue pour combattre la fracture numérique en proposant une approche ludique pour apprendre les bases de l'informatique à travers des mini-jeux, du contenu vidéo et textuel.\n" +
+      "## Pourquoi ce projet ?\n" +
+      "Même en 2024, de nombreuses personnes peuvent éprouver des difficultés à se servir d'un ordinateur ou d'un smartphone car elles n'ont jamais eu l'occasion de l'apprendre.\n\n" +
+      "Cela provoque des inégalités dans l’accès aux technologies, on appelle ce phénomène la fracture numérique.\n" +
+      "TechTutor a été conçu pour être une solution à ce problème.\n" +
+      "## Dans quel contexte ai-je travaillé sur ce projet ?\n" +
+      "Ce projet a été réalisé dans le cadre de la formation proposée par O'clock. Nous étions 5 à travailler dessus sur une période de 20 jours.\n" +
+      "## Quel est le public visé ?\n" +
+      "Tech Tutor cible un public diversifié, incluant ceux qui ont peu d'expérience avec les technologies ou qui se sentent exclus de la révolution numérique.\n\n" +
+      "L'application s'adresse particulièrement aux débutants de tous âges.\n" +
+      "## Quels défis avons-nous du relever ?\n" +
+      "- Nous avons du créer une interface simple et conviviale en réduisant au maximum le nombre de clics nécessaires pour effectuer une action.\n" +
+      "- Nous avons du faire en sorte que les utilisateurs ne soient jamais confrontés à une avalanche d'informations afin de pouvoir les faire progresser sans les effrayer.\n" +
+      "- Nous avons du trouver des solutions pour donner des indications aux utilisateurs quand aux progrès qu'ils font."
+    ,
     usedSkills: [
       typescript,
       nodejs,
@@ -24,6 +47,9 @@ export const projects: Project[] = [
       prisma,
       react,
       tailwind
+    ],
+    links: [
+      {text: "Voir le site", url: "https://techtutor.fr/"}
     ]
   },
   {
@@ -31,7 +57,7 @@ export const projects: Project[] = [
     name: "Truckrs",
     period: ["Janvier 2023", "Décembre 2023"],
     imagePath: "/projects/truckrs.png",
-    shortDescription: "Truckrs est une application qui permet la mise en relation de conducteur qualifiés avec des acteurs du monde du transport à la recherche de freelances.", 
+    shortDescription: "Truckrs est une application qui permet la mise en relation de conducteur qualifiés avec des acteurs du monde du transport à la recherche de freelances.",
     usedSkills: [
       typescript,
       nodejs,
@@ -39,6 +65,10 @@ export const projects: Project[] = [
       sequelize,
       dart,
       flutter
+    ],
+    links: [
+      {text: "App conducteurs", url: "https://app.truckrs.co/redirect-store"},
+      {text: "App transporteurs", url: "https://app.truckrs.co/redirect-store-pro"}
     ]
   },
 ];
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -3,26 +3,40 @@
 import { Footer } from "@/app/components/footer";
 import { LinkedParticles } from "@/app/components/linked-particles";
 import { NavMenu } from "@/app/components/nav-menu";
+import { OutlinedButton } from "@/app/components/outlined-button";
 import { ProjectCard } from "@/app/components/project-card";
+import { Title } from "@/app/components/title";
+import { montserratBold } from "@/app/lib/fonts";
 import { getProject } from "@/app/lib/projects";
 import { Section } from "@/app/lib/section";
 import { useRef } from "react";
+import Markdown from "react-markdown";
 
 type ProjectPageParams = {
   slug: string
 };
 
 export default function ProjectPage({params}: {params: ProjectPageParams}) {
-  const {slug, name, period, imagePath, usedSkills} = getProject(params.slug);
+  const {slug, name, period, imagePath, shortDescription, longDescription, usedSkills, links} = getProject(params.slug);
   
   const headerSection: Section = {
     title: "Résumé",
     ref: useRef<HTMLDivElement | null>(null)
   };
 
+  const descriptionSection: Section = {
+    title: "Le projet",
+    ref: useRef<HTMLDivElement | null>(null)
+  };
+
+  const linksSection: Section = {
+    title: "Liens",
+    ref: useRef<HTMLDivElement | null>(null)
+  };
+
   return (
     <div className="min-h-screen flex flex-col gap-8 justify-between">
-      <NavMenu sections={[headerSection]} />
+      <NavMenu sections={[headerSection, descriptionSection, linksSection]} />
       <main className="px-6 pt-20 pb-6 flex flex-col gap-8 items-center">
         <section ref={headerSection.ref} className="w-full flex flex-col gap-8 scroll-m-20">
           <ProjectCard
@@ -34,6 +48,38 @@ export default function ProjectPage({params}: {params: ProjectPageParams}) {
             showButton={false}
           />
         </section>
+        <section ref={descriptionSection.ref} className="w-full flex flex-col gap-3 scroll-m-20">
+          <Title text="Le projet" />
+          <div className="w-full p-4 rounded-3xl bg-gray-900/60">
+            <Markdown
+              components={{
+                h2: ({children}) => <h2 className={`text-lg mb-2 ${montserratBold.className}`}>{children}</h2>,
+                p: ({children}) => <p className="text-sm mb-2">{children}</p>,
+                ul: ({children}) => <ul className="text-sm mb-2">{children}</ul>,
+                li: ({children}) => (
+                  <div className="flex flex-row gap-2">
+                    <p>•</p>
+                    <p>{children}</p>
+                  </div>
+                )
+              }}
+            >
+              {longDescription ?? shortDescription}
+            </Markdown>
+          </div>
+        </section>
+        {
+          links && <section ref={linksSection.ref} className="w-full flex flex-col gap-3 scroll-m-20">
+            <Title text="Liens" />
+            {
+              links?.map((link, key) => (
+                <a key={key} className="w-full" href={link.url}>
+                  <OutlinedButton text={link.text} large={true} />
+                </a>
+              ))
+            }
+          </section>
+        }
       </main>
       <Footer
         name="Arnaud Petit"
